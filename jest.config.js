9f4eeda2c0e891dcc2083dbdd588e0f399002764
danny <danny_vcf@hotmail.com>
2019-06-15 10:35:12 -0300

    - Removed unnecessary configurations from jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,6 @@ module.exports = {
   testEnvironment: 'node',
   setupFilesAfterEnv: ["jest-extended"],
   testMatch: ['<rootDir>/test/**/*.test.{js,ts}'],
-  testPathIgnorePatterns: ["/lib/", "/node_modules/"],
   moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
   collectCoverage: true,
   collectCoverageFrom: [
